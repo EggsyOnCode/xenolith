dd73c2334b305210f737d30c039feb6e0ca78636
EggsyOnCode <farzansaqib2003@gmail.com>
2024-04-07 16:39:59 +0500

    added vm methods for pushing byte data i.e string and code

--- core/vm.go
+++ core/vm.go
@@ -3,8 +3,12 @@ package core
 type Instruction byte
 
 const (
-	InstrPushInt Instruction = 0x0a //10 (0-9 are reserved for ints)
-	InstrAdd     Instruction = 0x0b //11
+	InstrPushInt  Instruction = 0x0a // 10
+	InstrAdd      Instruction = 0x0b // 11
+	InstrPushByte Instruction = 0x0c
+	InstrPack     Instruction = 0x0d
+	InstrSub      Instruction = 0x0e
+	InstrStore    Instruction = 0x0f
 )
 
 type Stack struct {
@@ -18,6 +22,7 @@ func NewStack(size int) *Stack {
 		sp:   0,
 	}
 }
+
 func (s *Stack) Push(v any) {
 	s.data[s.sp] = v
 	s.sp++
@@ -27,12 +32,13 @@ func (s *Stack) Pop() any {
 	value := s.data[0]
 	s.data = append(s.data[:0], s.data[1:]...)
 	s.sp--
+
 	return value
 }
 
 type VM struct {
 	data  []byte
-	ip    int //instruction pointer
+	ip    int // instruction pointer
 	stack *Stack
 }
 
@@ -40,31 +46,47 @@ func NewVM(data []byte) *VM {
 	return &VM{
 		data:  data,
 		ip:    0,
-		stack: NewStack(100),
+		stack: NewStack(128),
 	}
 }
 
-// Run func that reads instructions from the stack register and processes them
 func (vm *VM) Run() error {
 	for {
-		instr := vm.data[vm.ip]
-		if err := vm.Exec(Instruction(instr)); err != nil {
+		instr := Instruction(vm.data[vm.ip])
+
+		if err := vm.Exec(instr); err != nil {
 			return err
 		}
+
 		vm.ip++
-		if vm.ip >= len(vm.data) {
+
+		if vm.ip > len(vm.data)-1 {
 			break
 		}
 	}
+
 	return nil
 }
 
 func (vm *VM) Exec(instr Instruction) error {
 	switch instr {
-	case (InstrPushInt):
-		//we have to explicity convert byte to int
-		vm.stack.Push(int(vm.data[vm.ip+1]))
-	case (InstrAdd):
+	case InstrPushInt:
+		vm.stack.Push(int(vm.data[vm.ip-1]))
+
+	case InstrPushByte:
+		vm.stack.Push(byte(vm.data[vm.ip-1]))
+
+	case InstrPack:
+		n := vm.stack.Pop().(int)
+		b := make([]byte, n)
+
+		for i := 0; i < n; i++ {
+			b[i] = vm.stack.Pop().(byte)
+		}
+
+		vm.stack.Push(b)
+
+	case InstrAdd:
 		a := vm.stack.Pop().(int)
 		b := vm.stack.Pop().(int)
 		c := a + b
--- core/vm_test.go
+++ core/vm_test.go
@@ -8,17 +8,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestVM(t *testing.T) {
+func TestVMAdd(t *testing.T) {
 	// 1 + 2 = 3
 	//push 1
 	//push 2
 	//add
 	//push 3
 
-	vm := NewVM([]byte{0x0a, 0x01, 0x0a, 0x02, 0x0b})
+	vm := NewVM([]byte{0x01, 0x0a, 0x02, 0x0a, 0x0b})
 
 	log.Printf("VM processing data : %v", vm.data)
 	assert.Nil(t, vm.Run())
 	fmt.Printf("Stack: %v\n", vm.stack)
 	assert.Equal(t, 3, vm.stack.Pop())
+}
+
+func TestVMByte(t *testing.T) {
+
 }
